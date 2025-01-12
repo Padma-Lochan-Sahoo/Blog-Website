73503f0ab33e853feeee39fe31805627186d5fb6
Padma-Lochan-Sahoo <padmalochansahoo8503@gmail.com>
2025-01-12 16:59:17 +0530

    Blog project

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
-import { configureStore } from "@reduxjs/toolkit";
-import authReducer from './authSlice.js'
+import {configureStore} from '@reduxjs/toolkit';
+import authSlice from './authSlice';
 
 const store = configureStore({
-    reducer:{
-        authReducer
+    reducer: {
+        auth : authSlice,
+        //TODO: add more slices here for posts
     }
-})
+});
 
-export default store
+
+export default store;
